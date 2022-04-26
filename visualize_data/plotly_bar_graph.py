5b0904b3b153692c7b6c64fb5f73c49e93c39793
Luis Lopez <luislopez@Luiss-MacBook-Pro.local>
2022-04-26 18:21:26 +0200

    Adding sctter graph script

--- visualize_data/plotly_bar_graph.py
+++ visualize_data/plotly_bar_graph.py
@@ -1,4 +1,3 @@
-
 import pandas as pd
 import plotly.graph_objects as go
 
@@ -7,9 +6,20 @@ DATASET = "nav_sykemeldte_alle_diagnoser.csv"
 
 
 if __name__ == "__main__":
-    df = pd.read_csv(f"{FOLDER}/{DATASET}")
+    # Gjør noen enkle transformasjoner
+    df = pd.read_csv(f"{FOLDER}/{DATASET}").replace("*", 0)
+    df["2019"] = df["2019"].astype(int)
+
+    # Lager figur
     fig = go.Figure()
-    bar_graph = go.Bar(x=df["Uke"], y=df["2019"])
+    bar_graph = go.Bar(x=df["Uke"], y=df["2019"], marker={"color": "#d7dbdb"})
     fig.add_trace(bar_graph)
 
+    # Styler figuren
+    fig.update_layout(paper_bgcolor="#526175",
+                      plot_bgcolor="#526175",
+                      yaxis={"color": "#d7dbdb", "tickfont": {"size": 20}},
+                      xaxis={"color": "#d7dbdb", "tickfont": {"size": 20}})
+
+    # Viser figuren i nettleseren
     fig.show()
---
+++ visualize_data/plotly_scatter_graph.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import plotly.graph_objects as go
+
+FOLDER = "data"
+DATASET = "nav_sykemeldte_alle_diagnoser.csv"
+
+
+if __name__ == "__main__":
+    # Gjør noen enkle transformasjoner
+    df = pd.read_csv(f"{FOLDER}/{DATASET}").replace("*", 0)
+    df["2019"] = df["2019"].astype(int)
+    df["2020"] = df["2020"].astype(int)
+
+    # Lager figur
+    fig = go.Figure()
+    line_graph_2019 = go.Scatter(x=df["Uke"],
+                                 y=df["2019"],
+                                 marker={"color": "#d7dbdb"},
+                                 line={"width": 10},
+                                 name="2019")
+
+    line_graph_2020 = go.Scatter(x=df["Uke"],
+                                 y=df["2020"],
+                                 marker={"color": "#cca39b"},
+                                 line={"width": 10},
+                                 name="2020")
+
+    fig.add_trace(line_graph_2019)
+    fig.add_trace(line_graph_2020)
+
+    # Styler figuren
+    fig.update_layout(paper_bgcolor="#526175",
+                      plot_bgcolor="#526175",
+                      yaxis={"color": "#d7dbdb", "tickfont": {"size": 20}},
+                      xaxis={"color": "#d7dbdb", "tickfont": {"size": 20}},
+                      legend={"font": {"color": "#d7dbdb", "size":20}}
+
+                      )
+
+    # Viser figuren i nettleseren
+    fig.show()
